7afc6c0a1b334cd7440ca2f1007613fe1d8da2ad
Jakkrit Junrat <j.jakkrit@appswin.co.th>
2018-03-07 23:12:21 +0700

    Prepair for Identity

---
+++ GpscWebApi.Scripts/Main/GetComponentsToDb.ps1
@@ -0,0 +1,24 @@
+﻿Import-Module -Name ($PSScriptRoot + "\LocusHelper.ps1")
+
+$PartnerId = "654156"
+
+$ClientId = "9300aee943490d96c73a5b177e5a2df6"
+$ClientSecret = "5dcdfbec7590646170dcbfd1b627fb5d"
+$Username = "VectorCuatroJapan"
+$Password = "welcome"
+
+Write-Host "Loging In..."
+$Authenticated = LocusAuthenticatePassword -ClientId $ClientId -ClientSecret $ClientSecret -Username $Username -Password $Password
+Write-Host "Login Success."
+$AccessToken = $Authenticated.access_token
+$RefreshToken = $Authenticated.refresh_token
+
+Write-Host "Retrieving the Coomponents..."
+$ComponentResponse = GetComponentsForPartner -AccessToken $AccessToken -PartnerId $PartnerId
+if ($ComponentResponse.statusCode -ne 200) {
+    Write-Error "Retrieve Components Fail."
+    return
+}
+$Components = $ComponentResponse.components
+
+$Components | ft
---
+++ GpscWebApi.Scripts/Main/LocusHelper.ps1
@@ -0,0 +1,22 @@
+﻿function LocusAuthenticatePassword($ClientId, $ClientSecret, $Username, $Password) {
+    $url = "https://api.locusenergy.com/oauth/token"
+    $GrantType = "password"
+    $Params =  @{
+        "grant_type" = $GrantType;
+        "client_id" = $ClientId;
+        "client_secret" = $ClientSecret;
+        "username" = $Username;
+        "password" = $Password;
+    }
+    $response = Invoke-WebRequest -Uri $url -Method Post -Body $Params -ContentType "application/x-www-form-urlencoded"
+    return $response | ConvertFrom-Json
+}
+
+function GetComponentsForPartner($AccessToken, $PartnerId) {
+    $url = "https://api.locusenergy.com/v3/partners/$($PartnerId)/components"
+    $HeaderParams = @{
+        "Authorization" = "Bearer $($AccessToken)"
+    }
+    $response = Invoke-WebRequest -Uri $url -Method Get -Headers $HeaderParams
+    return $response | ConvertFrom-Json
+}
