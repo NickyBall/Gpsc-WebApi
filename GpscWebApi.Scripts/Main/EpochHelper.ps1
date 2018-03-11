97d6002cc3d305af5751bc43ca00fe07379fad36
Jakkrit Junrat <j.jakkrit@appswin.co.th>
2018-03-11 16:06:01 +0700

    Weather Api Initialize

---
+++ GpscWebApi.Scripts/Main/EpochHelper.ps1
@@ -0,0 +1,8 @@
+﻿function FromUtcEpocTime ([long]$UnixTime)
+{
+    $epoch = New-Object System.DateTime (1970, 1, 1, 0, 0, 0, [System.DateTimeKind]::Utc);
+    return $epoch.AddSeconds($UnixTime);
+}
+$time = FromUtcEpocTime -UnixTime 1520758800
+$date = Get-Date $time
+Write-Host $date
---
+++ GpscWebApi.Scripts/Main/WeatherForecastFetcher.ps1
@@ -0,0 +1,10 @@
+﻿$app_id = "ca4877920e4c706e6087b469fff5af5b"
+
+function Call5Day3Hour($lat, $lon) {
+    $url = "http://api.openweathermap.org/data/2.5/forecast?lat=$($lat)&lon=$($lon)&appid=$($app_id)"
+    $response = Invoke-WebRequest -Uri $url -Method Get | ConvertFrom-Json
+    return $response
+}
+
+$weathers = Call5Day3Hour -lat 16.4282875 -lon 102.8339389
+$weathers | ft
